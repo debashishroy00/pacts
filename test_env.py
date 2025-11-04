184da7cdf97e2424f7274ead100c8cd5eb3838f4
sidroy2025 <debashishroy00@yahoo.com>
2025-11-03 23:19:49 -0500

    Update: Mon 11/03/2025 23:19:48.95

--- test_env.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-ok = load_dotenv()
-print("dotenv_loaded=", ok)
-for k in ["POSTGRES_HOST","POSTGRES_PORT","POSTGRES_DB","POSTGRES_USER","POSTGRES_PASSWORD"]:
-    print(k, "=", os.getenv(k))
